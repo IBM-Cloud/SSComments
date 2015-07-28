c0355492c5de24cf809d0973a0f167de5df59749
Jonathan Kaufman <jdkaufma@us.ibm.com>
2015-07-28 15:17:27 -0400

    enhancements to comment - link to actual post and user, add date

--- client/components/Comment.js
+++ client/components/Comment.js
@@ -3,10 +3,19 @@ var React = require('react');
 var Comment = React.createClass({
   render: function () {
     var comment = this.props.comment.value || this.props.comment.fields;
+    var date = new Date(comment.created*1000); // *1000 because reddit stores UNIX time
+    var linkToComment = 'https://www.reddit.com/r/SubredditSimulator/comments/' + comment.parent_id.substring(3) + '/'
+                        + comment.parent_id + '/' + this.props.comment.id;
     return (
-      <div className="comment">
-        <h5 className="comment-body">{'"' + comment.body + '"'}</h5>
-        <h6 className="comment-author">{'- '  + comment.author + ', ' + comment.score}</h6>
+      <div className='comment'>
+        <h5 className='comment-body'>
+          <a href={linkToComment} target='_blank'>{'"' + comment.body + '"'}</a>
+        </h5>
+        <h6 className='comment-info'>
+          <a className='comment-author' href={'https://www.reddit.com/user/' + comment.author + '?sort=top'} target='_blank'>{'- '  + comment.author}</a>
+          <span className='comment-score'>{', ' + comment.score}</span>
+          <span className='comment-date'>{date.toLocaleDateString() + ' ' + date.toLocaleTimeString()}</span>
+        </h6>
       </div>
     );
   }
