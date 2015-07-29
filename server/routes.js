13f2b9aff9872ebf7433aae0d507204834c6b911
Jonathan Kaufman <jdkaufma@us.ibm.com>
2015-07-29 14:06:18 -0400

    add endpoints for available insights and bot insights

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var db = require('./commentsdb');
+var commentsDB = require('./commentsdb');
+var insightsDB = require('./insightsdb');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -13,20 +14,47 @@ router.get('/comments', function (req, res) {
   var end = req && req.query && req.query.end;
   var prom;
   if (start && end) {
-    prom = db.searchAsync('ss_design', 'ss_created', {
+    prom = commentsDB.searchAsync('ss_design', 'ss_created', {
       limit: 100,
       q: 'created:[' + start + ' TO ' + end + ']',
       sort: '"-score<number>"'
     });
   } else {
-    prom = db.viewAsync('ss_design', 'ss_score', {descending: true, limit: 100});
+    prom = commentsDB.viewAsync('ss_design', 'ss_score', {descending: true, limit: 100});
   }
   prom.then(function (args) {
     res.json(args[0]);
   }).catch(function (e) {
     res.status(502);
-    res.json(err);
+    res.json(e);
   });
+});
+
+/** Get a list of the authors/bots that we have insights for */
+router.get('/availableinsights', function (req, res) {
+  insightsDB.viewAsync('insights_design', 'insights_id_to_rev', {}).then(function (args) {
+    var rows = args[0].rows;
+    res.json(rows.map(function (r) { return r.key; }));
+  }).catch(function (e) {
+    res.status(502);
+    res.json(e);
+  });
+});
+
+/** Get insights for a specific bot */
+router.get('/insights', function (req, res) {
+  var bot = req && req.query && req.query.bot;
+  if (!bot) {
+    res.status(400);
+    res.json(new Error('Must specify a bot'));
+  } else {
+    insightsDB.getAsync(bot, {}).then(function (args) {
+      res.json(args[0]);
+    }).catch(function (e) {
+      res.status(e.statusCode);
+      res.json(e);
+    })
+  }
 });
 
 module.exports = router;
